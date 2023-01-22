9c0945575d13e5ceba996b27ad25c7f668ce6f6b
Yuri3358 <yurialmeida917@gmail.com>
2023-01-22 20:40:24 -0300

    feat(template): finished

--- template/todoListTemplate.js
+++ template/todoListTemplate.js
@@ -1,13 +1,31 @@
 const todoListTemplate = `
 
-<div id="content">
+<div id="content" @keyup.enter="addTask" @keyup.esc="deleteTask">
     <header>Lista de Afazeres</header>
     <div id="form">
-        <label for="todo">Afazer</label> <br/>
-        <input id="todo" autocomplete=off> <br/>
-        <button class="btn btn-success">Adicionar</button>
-        <button class="btn btn-warning">Concluir</button>
-        <button class="btn btn-danger">Excluir</button>
+        <label for="todo">Tarefa</label> <br/>
+        <input id="todo" autocomplete=off ref="taskField" v-model="taskInput"> <br/>
+        <button class="btn btn-success" @click="addTask">Adicionar</button>
+        <button class="btn btn-warning" @click="completeTask">Concluir</button>
+        <button class="btn btn-danger" @click="deleteTask">Excluir</button>
+    </div>
+
+    <div id="table-container">
+        <table class="table table-striped table-secondary">
+            <thead>
+                <tr class="table-dark">
+                    <th>Tarefas</th>
+                    <th>Completa?</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="task in this.tasks.value">
+                    <td @click="this.taskInput = task.name">{{ task.name }}</td>
+                    <td>{{ task.completed }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
 `
