a7a37dee001ebb0deab473ad65ebb4b8d2e261e9
Yuri3358 <yurialmeida917@gmail.com>
2023-01-22 17:31:08 -0300

    feat: CRUD operations

--- component/todoList.js
+++ component/todoList.js
@@ -1,7 +1,58 @@
 const todoList = {
     template: todoListTemplate,
     data() {
-        
+        return {
+            todos: Vue.ref([]),
+            todoTitle: "",
+        }
+    },
+    mounted() {
+        this.getTodos()
+    },
+    methods: {
+        findTodo() {
+            let docRef
+            const search = this.todos.value.find(todo => todo.name == this.todoTitle)
+            if (search) {
+                docRef = colRef.doc(search.todoID)
+            }
+            return docRef
+        },
+        getTodos() {
+            let data = []
+            colRef.get().then(snapshot => {
+                snapshot.forEach(doc => {
+                    const todos = doc.data()
+                    todos.todoID = doc.id
+                    data.push(todos)
+                })
+            })   
+
+            this.todos.value = data
+            this.todoTitle = ""
+        },
+        async addTodo() {
+            if (this.todoTitle != "") {
+                await colRef.add({
+                    todo: this.todoTitle,
+                    completed: false
+                })
+            } else {
+                alert("Preencha o campo!")
+            }
+        }, 
+        async deleteTodo() {
+            if (this.todoTitle != "" && !(this.todoTitle in this.todos.value)) {
+                this.findTodo().delete()
+            } else {
+                alert("Selecione uma tarefa válida!")
+            }
+        },
+        async completeTodo() {
+            await this.findTodo().update({
+                completed: true
+            })
+        }
     }
 }
 
